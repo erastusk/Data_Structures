cda4656e9d4215c1e141e80872c4d6adf76d3d6a
erastusk <kj5574@yahoo.com>
2020-02-21 23:30:31 +0000

    pylint checks

--- binary_tree.py
+++ binary_tree.py
@@ -1,67 +1,34 @@
-""" 
-Binary Tree Traversals
-Depth First Search - Pre, In and Postorder
-Using notation n-node, l-left and r-right,
-pre - nlr
-in - lnr
-post - lrn
-
-On each node perform each check, eg post - lrn
-starting from root node 10
-do l, to 2, do l on 2 to 11
-on 11 do l, is null next do r is null next is n, print node
-go back to 2, l done do r to 12, l and r are null print node
-go back to 2, l and r done, n left, print node go back to 10
-l done do r, etc
-
-Pre
-10->2->11->12->6->2->20->
-
-In
-11->2->12->10->2->6->20->
-
-Post
-11->12->2->2->20->6->10->
-        
-            10
-     /               \  
-    2                 6
- /     \          /         \
- 11     12        2         20 
-"""
+"""Binary Tree Traversal """
 class Node():
-    def __init__(self,data):
+    """ Node definition """
+    def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
-
-
 class BinaryTree():
+    """ Binary Tree definition """
     def __init__(self, data):
         self.root = Node(data)
         self.pre = ""
         self.ino = ""
         self.post = ""
-    
     def preorder(self, start):
-        """Root->Left->Right"""
-        """Node->Left->Right"""
+        """Root->Left->Right
+        Node->Left->Right"""
         if start:
             self.pre += str(start.data) + "->"
             self.preorder(start.left)
             self.preorder(start.right)
-
     def inorder(self, start):
-        """Left->Root->Right"""
-        """Left->Node->Right"""
+        """Left->Root->Right
+        Left->Node->Right"""
         if start:
             self.inorder(start.left)
             self.ino += str(start.data) + "->"
             self.inorder(start.right)
-
     def postorder(self, start):
-        """Left->Right->Root"""
-        """Left->Right->Node"""
+        """Left->Right->Root
+        Left->Right->Node"""
         if start:
             self.postorder(start.left)
             self.postorder(start.right)
--- binary_tree_search.py
+++ binary_tree_search.py
@@ -1,84 +1,87 @@
-""" 
-Binary Tree Search
-        
-            8
-     /               \  
-    3                 10
- /     \          /         \
- 1      6        9          11 
-"""
+""" Binary Tree Insert and Search """
+
 class Node():
+    """ Node definition """
     def __init__(self,data):
         self.data = data
         self.left = None
         self.right = None
-
-
 class BinaryTree():
+    """ Binary Tree definition """
     def __init__(self, data):
         self.root = Node(data)
         self.pre = ""
         self.ino = ""
         self.post = ""
-   
     def search_BTS(self, data):
+        """Search Function"""
         root_node = self.root
-        
         if data > root_node.data:
             """ if greater then root right else left"""
             return self.search(data, root_node, "right")
-        else:
-            return self.search(data, root_node, "left")
-        
+        return self.search(data, root_node, "left")
     def search(self, data, root_node, direction):
+        """Search Helper Function"""
         if direction == "right":
-            if root_node == None:
+            if root_node is None:
                 return False
             if root_node.data == data:
                 return True
-            else:
-                return self.search(data, root_node.right, "right")
-
+            return self.search(data, root_node.right, "right")
         elif direction == "left":
-            if root_node == None:
+            if root_node is None:
                 return False
             if root_node.data == data:
                 return True
-            else:
-                return self.search(data, root_node.left, "left")
-        else:
-            return False
-    
+            return self.search(data, root_node.left, "left")
+        return False
     def insert_BTS(self, data):
         """ Insert node """
         insert_node = Node(data)
         if not self.root:
             self.root = insert_node
             return
-        self.insert_(insert_node, self.root)   
-    
+        self.insert_(insert_node, self.root)
     def insert_(self, insert_node, root_node):
         """ recursion insert method """
-        if root_node.right == None:
+        if root_node.right is None:
             root_node.right = insert_node
-            return
-        if insert_node.data == root_node.data:
+        elif insert_node.data == root_node.data:
             print "Node already exists"
-            return 
-        if root_node.left == None:
+        elif root_node.left is None:
             root_node.left = insert_node
-            return
-        if insert_node.data > root_node.data:
+        elif insert_node.data > root_node.data:
             return self.insert_(insert_node, root_node.right)
         elif insert_node.data < root_node.data:
             return self.insert_(insert_node, root_node.left)
-    
     def print_preorder(self):
+        """Print Binary Tree using Preorder traversal"""
         print(self.print_order(self.root, ""))
     def print_order(self, curr_node, traverse):
+        """Print helper function"""
         if curr_node:
             traverse += str(curr_node.data) + "->"
             traverse = self.print_order(curr_node.left, traverse)
             traverse = self.print_order(curr_node.right, traverse)
         return traverse   
+
+
+tree = BinaryTree(8)
+tree.root.left = Node(3)
+tree.root.left.right = Node(6)
+tree.root.left.left = Node(1)
+tree.root.right = Node(10)
+tree.root.right.left = Node(9)
+tree.root.right.right = Node(11)
+tree.print_preorder()
+print(tree.search_BTS(-1))
+tree.insert_BTS(80)
+tree.print_preorder()
+tree.insert_BTS(7)
+
+#tree.print_preorder()
+#tree.insert_BTS(8)
+#tree.print_preorder()
+#tree.insert_BTS(6)
+#tree.print_preorder()
 
